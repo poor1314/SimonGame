5cad61a8977d986b34a9bd051f6d398caf3e563a
poor1314 <junlinc9394@gmail.com>
2025-05-09 12:24:51 -0500

    game Logic complete

--- game.js
+++ game.js
@@ -1,18 +1,14 @@
-// add visual
-// 2. show game pattern 
-// pattern doesn't show correctly 
-
 const buttonColors = ["red", "blue", "green", "yellow"];
 const simonPattern = [];
 const userPattern = [];
 let round = 1;
 let index = 0; // might need to be naming this
+let lastIndex = 0;
 const blueButtonSound = new Audio('/sounds/blue.mp3');
 const greenButtonSound = new Audio("/sounds/green.mp3");
 const redButtonSound = new Audio("/sounds/red.mp3"); 
 const yellowButtonSound = new Audio("/sounds/yellow.mp3");
 const wrongChoiceSound = new Audio("/sounds/wrong.mp3")
-let animationComplete = false;
 
 function soundAnnouncement(sound){
     if (sound !== simonPattern[index]) return wrongChoiceSound.play();
@@ -37,26 +33,17 @@ document.addEventListener("keydown", e =>{
 document.addEventListener("click", e => {
     let userColor = e.target.id;
     let currentElement = e.target
-    
-    console.log("e.target:" , e.target);
-    console.log(userColor);
-    
     if (e.target.classList.contains("btn") && simonPattern.length > 0){
-        gameEffect(currentElement, "opacityChange");
+        gameEffect(currentElement, "opacityChange", 200);
         compareUserAndSimonPattern(userColor, currentElement);
     } 
-    console.log("simonPattern atClick", simonPattern);
+    // console.log("simonPattern atClick", simonPattern);
     console.log("userPattern atClick", userPattern);
 })
-
-
-
 
 function gameStart(e){
     if(e.key === " "){
         progressSimonPattern(round);
-        
-        console.log("Game Starts!",simonPattern);
         showAnnouncement(`Game Start! Current Round:${round}`)
     }
 }
@@ -65,55 +52,65 @@ function indexGenerate(){
    return Math.floor((Math.random() * 4));
 }
 
-function progressSimonPattern(round){
-    for(let i = 0; i < round; i++ ){
+function progressSimonPattern(){
+   
         simonPattern.push(buttonColors[indexGenerate()])
-        let color = document.querySelector("." + buttonColors[i]);
-        console.log("whats the color?", color);
+        console.log("index",index);
+        console.log("lastIndex",lastIndex);
         
-        gameEffect(color, "opacityChange");
-    }  
+        
+        let color = document.querySelector("." + simonPattern[lastIndex]);
+        
+        delayGameEffect(color);
+    console.log("Simon's patten", simonPattern);
 }
 
-function gameEffect(currentElement, message){
-    currentElement.classList.toggle(message);
+function delayGameEffect(color){
+    setTimeout(function() {
+        color.classList.toggle("opacityChange"); 
+    }, 200 * (lastIndex + 1));
+
+    setTimeout(function() {
+        // console.log(buttonColors[i]);
+        color.classList.toggle("opacityChange"); 
+    }, 200 * (lastIndex + 2));
+}
+
+
+function gameEffect(currentElement, effect, time){
+    currentElement.classList.toggle(effect);
     setTimeout(() => {
-        currentElement.classList.toggle(message);
-    }, 200);
+        currentElement.classList.toggle(effect);
+    }, time);
 }
 
 function compareUserAndSimonPattern(userColor){
-    showAnnouncement(`Current Round:${round}`)
-   
-
-    // if (buttonColors.includes(userColor)) soundAnnouncement(userColor);
+    showAnnouncement(`correct`)
+  
+    // play sounds
+    if (buttonColors.includes(userColor)) soundAnnouncement(userColor);
    
     if(userColor === simonPattern[index]){
         userPattern.push(simonPattern[index])
         index += 1;
+        
     }
     else{
         showAnnouncement(`Game lost! ${simonPattern[index]}(Simon) vs ${userColor}(you) \n press space to start new game!`)
         resetGameStats();
-        // document.body.classList.toggle(message)
+       
         document.body.style.backgroundColor = "red";
         setTimeout(() => {
             document.body.style.backgroundColor = "#011F3F";
         }, 200);
         return;
     }
-    // hasPlayerWonCurrentRound()
-    // replace the 1st if-statement with the above line when proceed to next round works
-    if(simonPattern.length === userPattern.length){
-        console.log("round won!");
-        
-        advanceToNextRound()
-        // showAnnouncement(`Current Round:${round}`)
-        // console.log(`current: ${round}`,simonPattern); 
-        // call nextRound()
-    }
-    if(simonPattern.length > 4){
+
+    if(userPattern.length >= 2){
         hasPlayerWonFullGame("Player won! press space to start a new game"); 
+    } else if(simonPattern.length === userPattern.length){
+        // console.log("round won!");
+        advanceToNextRound()
     }
 }
 
@@ -123,40 +120,26 @@ function hasPlayerWonFullGame(gameWonText){
 }
 
 function advanceToNextRound(){
+    lastIndex += 1;
     round += 1;
     index = 0;
-    simonPattern.length = 0; 
+    // simonPattern.length = 0;  
     userPattern.length = 0;
-    showAnnouncement(`Round Won! proceed to round:${round}`)
+    showAnnouncement(`Round Won! current round:${round}`)
     console.log("advanceToNextRound triggered!, current round: ", round);
     progressSimonPattern(round);
-    console.log(simonPattern); 
-    
+    // console.log(simonPattern); 
 }
-
-
 
 function resetGameStats(){
     index = 0; 
     round = 1; 
+    lastIndex = 0;
     simonPattern.length = 0; 
     userPattern.length = 0;
-}
-
-function visualAid(){
 }
 
 function showAnnouncement(gameStatus){
     document.querySelector("h1").textContent = gameStatus;
 }
-
-function freezeScreen(){
-}
-
-// const myTimeout = setTimeout(myGreeting, 5000);
-
-// function myStopFunction() {
-//   clearTimeout(myTimeout);
-// }
-
 
